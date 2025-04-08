510f64b1c12aa55741bea64647c60f8d1778d51c
JunimMotoboy <ypaulo816@gmail.com>
2025-04-08 13:55:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <h5>R$ ${valor} </h5>
           <div class"buttons">
           <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${modalId}">Visitar</button>
-          <button onclick="window.location.href='https://api.whatsapp.com/send?phone=${telefone}'" type="button" class="btn btn-success">
+          <button onclick="window.location.href='https://api.whatsapp.com/send?phone=${telefone}&text=Ola, tenho interesse no ${titulo}'" type="button" class="btn btn-success">
             Contato
           </button>
           </div>
@@ -133,6 +133,16 @@ document.addEventListener("DOMContentLoaded", () => {
     tamanho: "200mts",
     telefone: "55999999999"
   });
+  criarCard({
+    id: 3,
+    imagens: ["./img/lote-1.webp", "./img/lote-2.webp", "./img/lote-3.jpg"],
+    titulo: "Naked",
+    codigo: "TKBFF-022",
+    valor: "130.000,00",
+    tamanho: "200mts",
+    telefone: "55999999999"
+  });
+  
   criarCard({
     id: 3,
     imagens: ["./img/lote-1.webp", "./img/lote-2.webp", "./img/lote-3.jpg"],
